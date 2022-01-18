95af7537e60569d5933cca30f728b735f554f145
nmcb <marco@splatter.nl>
2022-01-18 15:12:20 +0100

    setup naive implementation using if expression and pattern matching

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,16 @@
 object Main extends App:
 
-  println("I was compiled with Scala 3")
+  def a1(m: Int, n: Int): Int =
+    if      (m == 0) n + 1
+    else if (n == 0) a1(m - 1, 1)
+    else             a1(m - 1, a1(m, n - 1))
+
+  def a2(m: Int, n: Int): Int =
+    (m, n) match
+      case (0, _) => n + 1
+      case (_, 0) => a2(m - 1, 1)
+      case (_, _) => a2(m - 1, a2(m , n - 1))
+
+  for (m <- 0 to 3 ; n <- 0 to 3)
+    println(s"a1($m, $n) = ${a1(m, n)}")
+    println(s"a2($m, $n) = ${a2(m, n)}")
