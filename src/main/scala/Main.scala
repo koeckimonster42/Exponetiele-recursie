72bf0c038a959ea96b4979ad2d08863de53eac0a
nmcb <marco@splatter.nl>
2022-04-21 20:20:28 +0200

    fixed test

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,13 @@
 object Main extends App:
 
-  def a1(m: Int, n: Int): Int =
-    if      (m == 0) n + 1
-    else if (n == 0) a1(m - 1, 1)
-    else             a1(m - 1, a1(m, n - 1))
+  import pure.*
 
-  def a2(m: Int, n: Int): Int =
-    (m, n) match
-      case (0, _) => n + 1
-      case (_, 0) => a2(m - 1, 1)
-      case (_, _) => a2(m - 1, a2(m , n - 1))
+  def a1(m: Int, n: Int): Pure[Int] =
+    if      (m == 0) done(n + 1)
+    else if (n == 0) call(a1(m - 1, 1))
+    else for {
+      inner <- call(a1(m, n - 1))
+      outer <- call(a1(m - 1, inner))
+    } yield outer
 
-  println(a1(4,1))
+  // println(a1(4,1).compute)
